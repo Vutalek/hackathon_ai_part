95afaf84a4de1a6ad13d1cd544e83c33f4bcaa6f
VutalekSK <mr.sergei02@mail.ru>
2025-12-12 02:37:05 +0300

    chat class added

---
+++ chat.py
@@ -0,0 +1,22 @@
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+
+class Chat:
+    def __init__(
+        self,
+        llm: BaseChatModel
+    ):
+        self.llm = llm
+        self.prompt = "Ты HR."
+        self.messages = []
+        self.messages.append(SystemMessage(self.prompt))
+
+    async def __call__(self, message: str) -> str:
+        self.messages.append(HumanMessage(message))
+        result = await self.llm.ainvoke(self.messages)
+        self.messages.append(result)
+        return str(result.content)
+    
+    def clear(self):
+        self.messages.clear()
